f0bc7c1ba2a47b61d3eca539948e56aa8ae349c9
ckenfack <kenfackkeng1995@gmail.com>
2024-04-05 18:42:16 +0100

    first commit

---
+++ src/test/java/org/acme/aws/one_jenkins_db/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme.aws.one_jenkins_db;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
